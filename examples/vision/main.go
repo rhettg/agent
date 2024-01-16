1b3b2023a9dce75d506afa8c6562a25fe20f4ddd
Rhett Garber <rhettg@gmail.com>
2024-01-16 15:31:17 -0800

    basic vision example

---
+++ examples/vision/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/rhettg/agent"
+	"github.com/rhettg/agent/provider/openaichat"
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+
+	p := openaichat.New(client, "gpt-4-vision-preview",
+
+		// By default, gpt-4-vision-preview is configured with a very small
+		// default max tokens. Make it bigger.
+		openaichat.WithMaxTokens(1024),
+	)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <image file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	a := agent.New(p)
+
+	imgName := os.Args[1]
+	imgData, err := os.ReadFile(imgName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading image file: %v\n", err)
+		os.Exit(1)
+	}
+
+	im := agent.NewImageMessage(agent.RoleUser, "please explain", imgName, imgData)
+	a.AddMessage(im)
+
+	m, err := a.Step(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error handling message: %v\n", err)
+		os.Exit(1)
+	}
+
+	content, err := m.Content(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting message content: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(content)
+}
